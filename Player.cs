944f0b07c1d5fd9c2e559c1e8d77e5b37bf0f050
Zak-K05 <zacharyk042@gmail.com>
2024-11-13 13:57:07 -0500

    Added player class

---
+++ Player.cs
@@ -0,0 +1,38 @@
+﻿using Game10003;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platformer
+{
+    public class Player
+    {
+        public Vector2 position = new Vector2(0, 0);
+        public int radius = 20;
+        public int speed = 5;
+
+        public void UpdatePlayer()
+        {
+            // Get the input for the player
+
+            if (Input.IsKeyboardKeyDown(KeyboardInput.A))
+            {
+                position.X -= speed;
+            }
+            else if (Input.IsKeyboardKeyDown(KeyboardInput.D))
+            {
+                position.X += speed;
+            }
+
+            // Draw the player
+
+            Draw.FillColor = Color.White;
+            Draw.LineSize = 1;
+
+            Draw.Circle(position, radius);
+        }
+    }
+}
