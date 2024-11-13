6720588924070418d2281af76784ab957ed5453d
Zak-K05 <zacharyk042@gmail.com>
2024-11-13 13:59:26 -0500

    Added platform class

---
+++ Platform.cs
@@ -0,0 +1,32 @@
+﻿using Game10003;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platformer
+{
+    public class Platform
+    {
+        public Vector2 position = new Vector2();
+        public Vector2 size = new Vector2();
+
+        public Platform(Vector2 pos, Vector2 s)
+        {
+            position = pos;
+            size = s;
+        }
+
+        public void UpdatePlatform()
+        {
+            Draw.FillColor = Color.Gray;
+            Draw.LineSize = 2;
+            Draw.LineColor = Color.Black;
+
+            Draw.Rectangle(position, size);
+
+        }
+    }
+}
