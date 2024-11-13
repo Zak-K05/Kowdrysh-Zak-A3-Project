6aa4a8732f85fa932246d89d327f45b89e6e5eb9
Zak-K05 <zacharyk042@gmail.com>
2024-11-13 14:02:10 -0500

    Added coin class

---
+++ Coin.cs
@@ -0,0 +1,33 @@
+﻿using Game10003;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platformer
+{
+    public class Coin
+    {
+        public bool active = true;
+        public Vector2 position;
+        public int size = 5;
+
+        public Coin(Vector2 pos)
+        {
+            position = pos;
+        }
+
+        public void UpdateCoin()
+        {
+            Draw.FillColor = Color.Yellow;
+            Draw.LineSize = 0;
+
+            if (active == true)
+            {
+                Draw.Circle(position, size);
+            }
+        }
+    }
+}
